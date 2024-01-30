40ecfc30085551865c7766517c43575b1976e6ef
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-01-31 00:32:02 +0900

    [준혁] 배틀메인윈도우에 상속구조 추가, UI 스크립트 위치 변경, 필요없는 코드 정리 (캔버스 세팅)
    .

--- Client/Assets/Scripts/Utility/Extension.cs
+++ Client/Assets/Scripts/Utility/Extension.cs
@@ -8,45 +8,20 @@ public static class Extension
 {
     public static T GetOrAddComponent<T>(this GameObject go) where T : UnityEngine.Component
     {
-        return Util.GetOrAddComponent<T>(go);
+        T component = go.GetComponent<T>();
+        if (component == null)
+            component = go.AddComponent<T>();
+
+        return component;
+    }
+
+    public static T ParseEnum<T>(string value)
+    {
+        return (T)Enum.Parse(typeof(T), value, true);
     }
 
     public static bool IsValid(this GameObject go)
     {
         return go != null && go.activeSelf;
-    }
-
-    public static void MakeMask(this ref LayerMask mask, List<Define.ELayer> list)
-    {
-        foreach (Define.ELayer layer in list)
-            mask |= (1 << (int)layer);
-    }
-
-    public static void AddLayer(this ref LayerMask mask, Define.ELayer layer)
-    {
-        mask |= (1 << (int)layer);
-    }
-
-    public static void RemoveLayer(this ref LayerMask mask, Define.ELayer layer)
-    {
-        mask &= ~(1 << (int)layer);
-    }
-
-    public static void DestroyChilds(this GameObject go)
-    {
-        foreach (Transform child in go.transform)
-            Managers.Resource.Destroy(child.gameObject);
-    }
-
-    public static void Shuffle<T>(this IList<T> list)
-    {
-        int n = list.Count;
-
-        while (n > 1)
-        {
-            n--;
-            int k = UnityEngine.Random.Range(0, n + 1);
-            (list[k], list[n]) = (list[n], list[k]); //swap
-        }
     }
 }
