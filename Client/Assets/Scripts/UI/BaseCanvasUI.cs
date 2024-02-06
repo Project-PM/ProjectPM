187f725d7695b56690639378cca4338db9be4b51
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-02-06 18:05:19 +0900

    일단 캔버스 비율 고정 (1920, 1080)

--- Client/Assets/Scripts/UI/BaseCanvasUI.cs
+++ Client/Assets/Scripts/UI/BaseCanvasUI.cs
@@ -4,26 +4,37 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class UIParam { }
 
 public class BaseCanvasUI : InitBase
 {
     [SerializeField] private Canvas canvas;
     [SerializeField] private CanvasScaler canvasScaler;
 
-    protected virtual void Reset()
+    public override bool Init()
     {
+        if (base.Init() == false)
+            return false;
+
         canvas = this.GetOrAddComponent<Canvas>();
         canvasScaler = this.GetOrAddComponent<CanvasScaler>();
 
         this.gameObject.layer = LayerMask.NameToLayer("UI");
+
         SetCanvas();
         SetCanvasScaler();
+        SetResolution();
+
+        return true;
     }
 
     private void SetCanvas()
     {
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
         canvas.overrideSorting = true;
+        canvas.worldCamera = Camera.main;
         canvas.sortingOrder = 0;
 
         canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.None;
@@ -34,8 +45,38 @@ public class BaseCanvasUI : InitBase
     {
         canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
         canvasScaler.referenceResolution = new Vector2(1920, 1080);
-        canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+        canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
         canvasScaler.referencePixelsPerUnit = 100;
+        canvasScaler.matchWidthOrHeight = 1.0f;
+    }
+
+    public void SetResolution()
+    {
+        int setWidth = 1920; // 사용자 설정 너비
+        int setHeight = 1080; // 사용자 설정 높이
+
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+
+        OnPreCull();
+    }
+
+    private void OnPreCull()
+    {
+        GL.Clear(true, true, Color.black);
     }
 
     protected void SetSortingOrder(int order)
@@ -44,4 +85,3 @@ public class BaseCanvasUI : InitBase
     }
 }
 
-public class UIParam { }
