7136af73989e58b94349e8c17a7b6d1ac6891a8e
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-02-23 21:28:39 +0900

    파베데이터구조 잡는 중...?

--- Client/Assets/Scripts/Managers/FBDataManager.cs
+++ Client/Assets/Scripts/Managers/FBDataManager.cs
@@ -5,6 +5,7 @@ using Firebase;
 using Firebase.Database;
 using Newtonsoft.Json;
 using System;
+using JetBrains.Annotations;
 
 #region FBUserDatas
 /// <summary>
@@ -14,18 +15,21 @@ public enum FirebaseDataType
 {
     UserInfo,
     UserItem,
+    Max,
 }
 
 /// <summary>
 /// 유저 데이터 베이스 저장용 구조,
 /// 필요한 경우에는 UserData 클래스 종류를 늘려줄 것
 /// </summary>
+[Serializable]
 public class FBUserData
 {
     public FBUserInfo userInfo = new FBUserInfo();
     public FBUserItem userItem = new FBUserItem();
 }
 
+[Serializable]
 public class BaseFBData { }
 
 [Serializable]
@@ -36,7 +40,6 @@ public class FBUserInfo : BaseFBData
     public string userNickName = string.Empty; // 유저 닉네임 (초기엔 유저키로 세팅)
     public bool isLogined = false; // 현재 로그인 상태 (중복 로그인 관련 처리?)
     public UserLoginType userLoginType = UserLoginType.Guest; // 계정 동기화 정보
-
 }
 
 [Serializable]
@@ -46,10 +49,19 @@ public class FBUserItem : BaseFBData
 }
 #endregion
 
-public class PlatformManager
+public class FBDataManager
 {
     FBUserData fbDataInfo = new FBUserData();
+    IPlatformDB userDataBase;
+
+    private string userIDToken = "";
+    public string UserIDToken
+    {
+        get
+        {
+            return userIDToken;
+        }
+    }
 
     
-   
 }
